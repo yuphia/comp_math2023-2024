4903cfa7eb2b39e208761d291de5b0e071e5eaea
yuphia <grentubedark@gmail.com>
2023-11-08 16:27:01 +0300

    siedel

--- lab2/task2.py
+++ lab2/task2.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import time
 
 ################################################################ helper functions #######################################################################
 
@@ -47,32 +48,74 @@ def subtract_lines(matrix, first, second):
 def subtract_lines_divided(matrix, first, second, number):
     matrix[first] = np.add(matrix[first], np.multiply(matrix[second], np.full((1, len(matrix[second])), -1/number)))
     return matrix
+
+def split_LDU(matrix):
+    LDU = []
+    L = np.zeros_like(matrix)
+    D = np.zeros_like(matrix)
+    U = np.zeros_like(matrix)
+
+    for i in range(0, len(matrix)):
+        D[i][i] = matrix[i][i]
+    for i in range(0, len(matrix)):
+        for j in range(i+1, len(matrix)):
+            U[i][j] = matrix[i][j]
+
+    for i in range(0, len(matrix)):
+        for j in range(0, i):
+            L[i][j] = matrix[i][j]
+
+    LDU.append(L)
+    LDU.append(D)
+    LDU.append(U)
+    return LDU
+
+def discrepancy(matrix, right_side, u):
+    Af = np.dot(matrix, u)
+    discrepancy = np.zeros_like(right_side)
+    for i in range(len(right_side)):
+        discrepancy[i] = right_side[i]-Af[i]
+
+    norm = 0.0
+    for i in range(len(right_side)):
+        norm += discrepancy[i]*discrepancy[i]
+
+    return math.sqrt(norm)
+
+def iteration_body(matrix, LplusU, Dinv, u_current, right_side):
+    i = 0
+    arr = [[], []]
+    discr = 1
+    while (discr > 1e-10):
+        u_current = -1*np.dot(np.dot(Dinv, LplusU), u_current) + np.dot(Dinv, right_side)
+        i+=1
+        discr = discrepancy(matrix, right_side, u_current)
+        arr[0].append(i)
+        arr[1].append(discr)
+    
+    plt.plot(arr[0], arr[1], marker='o')
+    plt.show()
+    return u_current
+
 ################################################################ methods #######################################################################
 
 def gaussian_method(matrix, right_side):
     system = concat_right_side_to_matrix(matrix, right_side)    
     for i in range(0, len(system)):
-        print(system)
         system = rotation_for_gauss(system, i)
         for j in range(i+1, len(system)):
             if(system[j][i] != 0):
                 system = subtract_lines_divided(system, j, i, system[i][i]/system[j][i])
 
-    print(system)
     sol = np.zeros(len(right_side))
     for i in range(len(system)-1, -1, -1):
-        print(i)
         accum = system[i][len(system[i])-1]
         for j in range(i+1, len(system)):
             accum -= system[i][j]*sol[j]
-        
-        print(accum)
+    
         system[i][len(system)] = accum
 
-        print("i end = ", system[i][len(system)])
-        print("i, i = ", system[i][i])
         sol[i] = system[i][len(system)]/system[i][i]
-        print(sol)
 
     return sol
     
@@ -87,17 +130,53 @@ def LU_decomposition(matrix, right_side):
     print(matrix)
     print(np.dot(L, matrix))
 
+def seidel(matrix, right_side):
+    u = np.zeros_like(right_side)
+    LDU = split_LDU(matrix)
+    
+    Dinv = np.zeros_like(matrix)
+    for i in range(len(matrix)):
+        Dinv[i][i] = 1/LDU[1][i][i]
+
+    LplusU = np.zeros_like(matrix)
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            LplusU[i][j] = LDU[0][i][j]
+
+    for i in range(len(matrix)):
+        for j in range(i+1, len(matrix)):
+            LplusU[i][j] = LDU[2][i][j]
+
+    solution = iteration_body(matrix, LplusU, Dinv, u, right_side)
+    return solution
+    
+
 ################################################################ testing code #######################################################################
-a = np.array([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 8.0]])
-b = np.array([[1.0], [1.0], [1.0]])
 
-LU_decomposition(a, b)
+b_for_iters = np.zeros(100)
+b = np.zeros((100, 1))
+for i in range(0, 100):
+    b_for_iters[i] = 100-i
+    b[i][0] = 100-i
 
-A = np.array([[50.0, 70.0], [70.0, 101.0]])
-print(A)
-print(np.dot(A, A))
-print(np.linalg.inv(A))
-print(np.linalg.eig(np.dot(A, A)))
-print(np.linalg.eig(np.dot(np.linalg.inv(A), np.linalg.inv(A))))
+print(b_for_iters)
+print(b)
+a = np.zeros((100, 100))
 
-print(1.0/2.25e+04)
+for i in range(0, 100):
+    a[0][i] = 1
+
+for i in range(1, 99):
+    for j in range(i-1, i+2):
+        if(i!=j):
+            a[i][j] = 1
+        else:
+            a[i][j] = 10
+
+a[99][98] = 1
+a[99][99] = 1
+
+solution = gaussian_method(a, b)
+print("Невязка для метода Гаусса с выбором главного элемента =", discrepancy(a, b, solution))
+
+solution = seidel(a, b_for_iters)
